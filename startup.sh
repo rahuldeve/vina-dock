68e50d1301c63a7d96e202a48af0a026e8432f9a
rdedev <rahul.e.dev@gmail.com>
2024-01-11 00:46:38 +0000

    add startup script

---
+++ startup.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install autodock-vina g++
+# install python 3.8
+pip install vina
+pip install --upgrade deepchem networkx pdbind firebase-admin  
